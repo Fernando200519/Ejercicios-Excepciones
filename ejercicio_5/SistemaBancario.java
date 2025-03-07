c355fd0343dfc236801979fd67864450e2bf01bf
Fernando200519 <zs23021811@estudiantes.uv.mx>
2025-03-06 22:09:17 -0600

    segundo commit

---
+++ ejercicio_5/SistemaBancario.java
@@ -0,0 +1,13 @@
+// Descripción: Implementar un pequeño sistema bancario que maneje diferentes tipos de excepciones 
+//              organizadas en una jerarquía. El sistema debe permitir crear cuentas, realizar depósitos, retiros y 
+//              transferencias entre cuentas.
+
+// Entrada: Comandos para crear cuentas, depositar, retirar y transferir dinero.
+// Salida: Confirmación de operaciones exitosas o mensajes descriptivos según la excepción capturada, con 
+//          manejo de múltiples niveles de try-catch y finally para garantizar la consistencia del sistema.
+
+package ejercicio_5;
+
+public class SistemaBancario {
+    
+}
